623ccd9ce98179a2ebefb6054725434595298685
youhoushun <526542006@qq.com>
2017-11-22 16:02:05 +0800

    simple print

--- Hello.java
+++ Hello.java
@@ -1,12 +1,7 @@
 public class Hello{
-	public static final String COMPANY_NAME ="youhoushun";
 	public static void main(String args[]){
-		system.out.print("happy new year");
-		system.out.print("happy myself");
-		print();
+		System.out.print("***************");
+		System.out.print("###############");
 	}
-	public static void print(){
-		Sysout.out.print("***********");
-		Sysout.out.print("Hello World!");
-	}
+
 }
