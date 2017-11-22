9a7b8335209777fd34b21778d1d36260859511c5
youhoushun <526542006@qq.com>
2017-11-22 15:55:50 +0800

    add static method

--- Hello.java
+++ Hello.java
@@ -2,5 +2,10 @@ public class Hello{
 	public static void main(String args[]){
 		system.out.print("happy new year");
 		system.out.print("happy myself");
+		print();
+	}
+	public static void print(){
+		Sysout.out.print("***********");
+		Sysout.out.print("Hello World!");
 	}
 }
