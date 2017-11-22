c8d999db39211ed7976b069fd7f854694e7668d1
youhoushun <526542006@qq.com>
2017-11-22 16:03:51 +0800

    hello print

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 	public static void main(String args[]){
-		System.out.print("***************");
-		System.out.print("###############");
+		System.out.print("Hello World");
+		System.out.print("Hello youhoushun");
 	}
 
 }
