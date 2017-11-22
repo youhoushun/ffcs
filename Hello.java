08309736ce4bbbc3d3872f68ffa0c71a40498377
youhoushun <526542006@qq.com>
2017-11-22 15:52:08 +0800

    add static attribute

--- Hello.java
+++ Hello.java
@@ -1,4 +1,5 @@
 public class Hello{
+	public static final String COMPANY_NAME ="youhoushun";
 	public static void main(String args[]){
 		system.out.print("happy new year");
 		system.out.print("happy myself");
